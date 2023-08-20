15db53f9c11e0b0e8fc4f9d5bbf5318dc2f12e09
Anastasia <didyoumissme.19s@gmail.com>
2023-08-20 23:41:41 +0300

    tast4

---
+++ tast4/Program.cs
@@ -0,0 +1,4 @@
+﻿//Задача 4: Поиск уникальных элементов в списке. Напишите программу, которая создает новый список, содержащий только уникальные элементы из //исходного списка.
+var all = new[] {25,25,12,13,12,4}.ToList();
+var listen = all.GroupBy(i => i).Where(i => i.Count() == 1).SelectMany(i => i);
+System.Console.WriteLine("[" + string.Join(",", listen)+"]");
