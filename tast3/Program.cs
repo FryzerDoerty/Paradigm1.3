f690bdf26b83b0a38189432faa185d39b2e65440
Anastasia <didyoumissme.19s@gmail.com>
2023-08-20 23:00:42 +0300

    Decl 1

---
+++ tast3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 3: Вычисление факториала числа. Напишите программу, которая находит факториал заданного числа N с использованием рекурсии или встроенных 
+//функций. Декларативный
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+    return n * Factorial(n - 1);
+}
+System.Console.WriteLine("Введите число для нахождения факториала: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = Factorial(n);
+System.Console.WriteLine(a);
